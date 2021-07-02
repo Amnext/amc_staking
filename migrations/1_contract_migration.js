d7966719124ff9efc736b103388070752a41f65f
RaufGad <raufgad@gmail.com>
2021-07-02 09:36:37 +0800

    update migration script

--- migrations/1_contract_migration.js
+++ migrations/1_contract_migration.js
@@ -1,4 +1,5 @@
 const AMCToken = artifacts.require("AMCToken");
+const AMCStaking = artifacts.require("AMCStaking");
 
 const DEV_WALLET = "0x362C378e0855545C521b62bdEDeff54a8a4C677A";
 const MANAGER_WALLET = "0xDF40cD1dA7045E855F7C0e351475F49f810a04a7";
@@ -7,4 +8,7 @@ const BNB_SPONSOR = "0xFd6a98cD1a84713A74F8Aa8869537b20dfFFd515";
 module.exports = async function (deployer) {
   await deployer.deploy(AMCToken, DEV_WALLET, MANAGER_WALLET, "100000000000000000000", BNB_POOL_ADDRESS, BNB_SPONSOR);
   const token = AMCToken.deployed();
+  await deployer.deploy(AMCStaking, token.address, "6000000000000000000")
+  const staking = AMCStaking.deployed();
+  await token.setStaking(staking.address)
 };
