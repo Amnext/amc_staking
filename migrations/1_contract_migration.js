b8f9a0131748f5e64caa2bbab8c746dc1ff14e4b
RaufGad <raufgad@gmail.com>
2021-07-13 10:56:03 +0800

    update migration for BNB pool info

--- migrations/1_contract_migration.js
+++ migrations/1_contract_migration.js
@@ -3,8 +3,8 @@ const AMCStaking = artifacts.require("AMCStaking");
 
 const DEV_WALLET = "0x22905BdFd65cEE48882c68a7761A89C9B8F0494D";
 const MANAGER_WALLET = "0x6C44eDacc815Cd625ca7191fF74D30B9c72104a3";
-const BNB_POOL_ADDRESS = "0xbccd4a3c8df54c887e5742fba5dc2a6f0c701f59";
-const BNB_SPONSOR = "0xfd6a98cd1a84713a74f8aa8869537b20dfffd515";
+const BNB_POOL_ADDRESS = "0x2dc249000a4707de35d8be2f606d9863b1e327f9";
+const BNB_SPONSOR = "0xf2c23c72a1a1db0e8664f7fdfee0e7ea20c2bb96";
 module.exports = async function (deployer) {
   await deployer.deploy(AMCToken, DEV_WALLET, MANAGER_WALLET, "40000000000000000000", BNB_POOL_ADDRESS, BNB_SPONSOR);
   const token = await AMCToken.deployed();
